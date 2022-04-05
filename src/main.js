abcdaa4f17fcc40d1bfd6176fc66603d7ee4f198
Cesar Esquivel <czr.esquivel@gmail.com>
2022-04-04 19:34:29 -0500

    another solution for working with numbers

--- src/main.js
+++ src/main.js
@@ -1,9 +1,9 @@
 let x = 2;
 let y = 5;
 
-console.log(x + y);
-console.log(x - y);
-console.log(x * y);
-console.log(x / y);
-console.log (x % y);
-console.log(x ** y);
+console.log(x > y);
+console.log(x >= y);
+console.log(x < y);
+console.log(x <= y);
+console.log (x == y);
+console.log(x != y);
